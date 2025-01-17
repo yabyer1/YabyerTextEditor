897511c7ccbe9e061cc9fe05ebea8887c053bf7a
Abhay Yer <abhay.iyer7@gmail.com>
2025-01-17 12:47:12 -0600

    save file

--- Project/app/src/main/java/org/example/app/App.java
+++ Project/app/src/main/java/org/example/app/App.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static java.awt.Cursor.HAND_CURSOR;
 
@@ -19,32 +21,69 @@ public class App {
         frame.add(panel);
        JMenuBar menuBar = new JMenuBar();
        JMenu fileMenu = new JMenu("File");
-       JMenuItem openItem = new JMenuItem("Open....");
-       openItem.addActionListener(e ->{
-           JFileChooser fileChooser = new JFileChooser();
-           int res = fileChooser.showOpenDialog(frame);
-           if(res == JFileChooser.APPROVE_OPTION) {
-               File selectedFile = fileChooser.getSelectedFile();
+       JMenuItem openItem = getJMenuItem(frame, panel);
+       fileMenu.add(openItem);
 
-               if (selectedFile != null) {
-                   try {
-                       panel.gapbuffer.LoadFile(selectedFile.getAbsolutePath(), panel.f);
-                       panel.repaint();
-                   } catch (IOException ex) {
-                       ex.printStackTrace();
-                       JOptionPane.showMessageDialog(frame, "Error loading file" + ex.getMessage()
-                               , "File Load Error", JOptionPane.ERROR_MESSAGE);
-                   }
-
-               }
-           }
-           });
-            fileMenu.add(openItem);
-            menuBar.add(fileMenu);
-            frame.setJMenuBar(menuBar);
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+       JMenuItem saveAsItem = new JMenuItem("Save As....");
+       saveAsItem.addActionListener(e ->{
+           saveFileAs(frame, panel);
+       });
+       fileMenu.add(saveAsItem);
+       menuBar.add(fileMenu);
+       frame.setJMenuBar(menuBar);
+       frame.pack();
+       frame.setLocationRelativeTo(null);
+       frame.setVisible(true);
         });
+    }
+
+    private static void saveFileAs(JFrame frame, EditorPanel panel) {
+        JFileChooser fileChooser = new JFileChooser();
+        int res = fileChooser.showSaveDialog(frame);
+        if(res == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+
+            if (selectedFile != null) {
+                try {
+                   String text = panel.gapbuffer.getText();
+                   Files.write(Paths.get(selectedFile.getAbsolutePath()), text.getBytes());
+                    JOptionPane.showMessageDialog(
+                            frame,
+                            "File saved successfully!",
+                            "Save As",
+                            JOptionPane.INFORMATION_MESSAGE
+                    );
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(frame, "Error saving file" + ex.getMessage()
+                            , "File Save Error", JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+        }
+    }
+
+    private static JMenuItem getJMenuItem(JFrame frame, EditorPanel panel) {
+        JMenuItem openItem = new JMenuItem("Open....");
+        openItem.addActionListener(e ->{
+            JFileChooser fileChooser = new JFileChooser();
+            int res = fileChooser.showOpenDialog(frame);
+            if(res == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+ 
+                if (selectedFile != null) {
+                    try {
+                        panel.gapbuffer.LoadFile(selectedFile.getAbsolutePath(), panel.f);
+                        panel.repaint();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(frame, "Error loading file" + ex.getMessage()
+                                , "File Load Error", JOptionPane.ERROR_MESSAGE);
+                    }
+
+                }
+            }
+            });
+        return openItem;
     }
 }
